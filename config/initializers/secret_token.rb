d534c506422df0c9fbc5018e968fb7ab2e0ebfab
morinoko <forby.1@gmail.com>
2014-04-16 21:53:34 +0900

    add home, about, and contact pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kingoma::Application.config.secret_key_base = '2cb7b5fbc7d4110f13b89c39484cec7c2b30a0162683fececb1d54e1766608ba00d6725344f29cba8dd8cb4dc2984d51cd7c2946501f0b954307be6968bd750a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kingoma::Application.config.session_store :encrypted_cookie_store, key: '_kingoma_session'
